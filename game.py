ad413ee81e3520cf339c81748d20c690e843d766
isurumanoddev <isurumanod99@gmail.com>
2022-12-02 05:45:56 +0530

    create Snake object in the Snake class

--- game.py
+++ game.py
@@ -11,7 +11,16 @@ FOOD_COLOR ="red"
 
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size=BODY_PARTS
+        self.coordinates=[]
+        self.squares=[]
+
+        for i in range(0,BODY_PARTS):
+            self.coordinates.append([0,0])
+        for x,y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+            self.squares.append(square)
 
 
 class Food:
@@ -50,5 +59,8 @@ label.pack()
 
 canvas = Canvas(window, height=GAME_HEIGHT, width=GAME_WIDTH, background="black")
 canvas.pack()
+
+food = Food()
+snake = Snake()
 
 window.mainloop()
