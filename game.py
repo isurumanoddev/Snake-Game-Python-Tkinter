1d33fcbbb065bcd9eedef5739e7ff946879355b9
isurumanoddev <isurumanod99@gmail.com>
2022-12-02 05:38:06 +0530

    crete the game window.

---
+++ game.py
@@ -0,0 +1,51 @@
+from tkinter import *
+import random
+
+GAME_WIDTH=1000
+GAME_HEIGHT=700
+SPEED =400
+SPACE_SIZE =25
+BODY_PARTS =4
+SNAKE_COLOR ="yellow"
+FOOD_COLOR ="red"
+
+
+class Snake:
+    pass
+
+
+class Food:
+    def __init__(self):
+        x= random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y= random.randint(0, (GAME_HEIGHT/ SPACE_SIZE) - 1) * SPACE_SIZE
+
+
+def next_turn():
+    pass
+
+
+def change_direction():
+    pass
+
+
+def check_collisions():
+    pass
+
+
+def game_over():
+    pass
+
+
+window = Tk()
+
+score = 0
+direction ="down"
+
+
+label = Label(window, text="Score :{}".format(score), font=("consolas", 30))
+label.pack()
+
+canvas = Canvas(window, height=GAME_HEIGHT, width=GAME_WIDTH, background="black")
+canvas.pack()
+
+window.mainloop()
